7c49712a2774b1fcf3704cfd7300fb575a015c9c
Andrea Cotza <andrea@cotza.me>
2015-08-19 21:35:02 +0200

    Update to v0.4.2
    - Added support for debian 7 (wheezy)
    - Added support for sub-directories

--- sources/lets-chat/node_modules/lets-chat-ldap/node_modules/passport-ldapauth/lib/passport-ldapauth/index.js
+++
@@ -1,7 +0,0 @@
-var Strategy = require('./strategy');
-
-// Export directly from package since there is nothing else to export
-exports = module.exports = Strategy;
-
-// Export constructor like before for compatibility
-exports.Strategy = Strategy;
